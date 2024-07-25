cd5f2a8a15285ad3d13a874203a92fb88307111e
RYN BSD <rynbsd04@gmail.com>
2024-07-25 08:49:24 +0100

    add auth swagger

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -11,16 +11,130 @@ const { handleAsync } = util.fn;
 const { isUnauthenticated, isAuthenticated } = middleware.fn;
 const { signUp, signIn, signOut, me, forgotPassword, resetPassword, status } = controller.auth;
 
+/**
+ * @openapi
+ * 
+ * /auth/sign-up:
+ *  post:
+ *    summary: Create a new account(user)
+ *    tags:
+ *      - auth
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              phone:
+ *                type: string
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *    responses:
+ *      201:
+ *        description: Created successfully
+ *      4xx:
+ *        description: Client error
+ *      5xx:
+ *        description: Server error
+ */
 auth.post("/sign-up", handleAsync(isUnauthenticated), handleAsync(upload.none()), handleAsync(signUp));
 
+/**
+ * @openapi
+ * 
+ * /auth/sign-in:
+ *  post:
+ *    summary: Sign in to an account
+ *    tags:
+ *      - auth
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: Signed in successfully
+ *      4xx:
+ *        description: Client error
+ *      5xx:
+ *        description: Server error
+ */
 auth.post("/sign-in", handleAsync(isUnauthenticated), handleAsync(upload.none()), handleAsync(signIn));
 
+/**
+ * @openapi
+ * 
+ * /auth/sign-out:
+ *  post:
+ *    summary: Sign out from account
+ *    tags:
+ *      - auth
+ *    responses:
+ *      200:
+ *        description: Signed out successfully
+ *      4xx:
+ *        description: Client error
+ *      5xx:
+ *        description: Server error
+ */
 auth.post("/sign-out", handleAsync(isAuthenticated), handleAsync(upload.none()), handleAsync(signOut));
 
+/**
+ * @openapi
+ * 
+ * /auth/me:
+ *  post:
+ *    summary: Authorization
+ *    tags:
+ *      - auth
+ *    parameters:
+ *      - in: header
+ *        name: Authorization
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Signed in successfully
+ *      4xx:
+ *        description: Client error
+ *      5xx:
+ *        description: Server error
+ */
 auth.post("/me", handleAsync(upload.none()), handleAsync(me));
 
 auth.post("/forgot-password", handleAsync(upload.none()), handleAsync(forgotPassword));
 
 auth.put("/reset-password", handleAsync(upload.none()), handleAsync(resetPassword));
 
+/**
+ * @openapi
+ * 
+ * /auth/status:
+ *  post:
+ *    summary: Check status of user if he is still signed in or not
+ *    tags:
+ *      - auth
+ *    responses:
+ *      200:
+ *        description: User still signed in
+ *      401:
+ *        description: User signed out
+ *      4xx:
+ *        description: Client error
+ *      5xx:
+ *        description: Server error
+ */
 auth.get("/status", handleAsync(status));
